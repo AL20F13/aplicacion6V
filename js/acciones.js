cfd80037b62c049987aad7398268391dd1feb08d
AL20F13 <duranc96@gmail.com>
2015-04-29 14:49:47 -0500

    dos
    vibrar

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,22 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
-	$('#btnvibrar').on('tap',function() {
-		navigator.vibrate(1000);
-	}); //tap a btnvibrar
-	
-	$('#btnbeep').on('tap',function() {
-		navigator.beep(1);
-		}); //tap a btnbeep
+	 $('#btnvibrar').on('tap',function(){
+		 navigator.vibrate(1000);
+	 });//tap a btnvibrar
+	 
+	 $('#btnbeep').on('tap',function(){
+		 navigator.beep(1);
+	 });//tap a btnbeep
+	 
+	  
+	 $('#izquierda').on('swipeleft',function(){
+		alert("barrio a la izquierda");
+	 });//tap a izquierda
+	 
+	  $('#derecha').on('swiperight',function(){
+		alert("barrio a la derecha");
+	 });//tap a derecha
 }); 
 });
 
